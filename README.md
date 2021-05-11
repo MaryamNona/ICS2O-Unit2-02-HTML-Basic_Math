88b163c68e468bab82ba72e02d948715247f3324
MaryamNona <>
2021-05-11 22:21:21 +0000

    added JS

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ICS2O-Unit2-02-HTML-Basic_Math
+
+[![GitHub's Super Linter](https://github.com/MaryamNona/ICS2O-Unit2-02-HTML-Basic_Math/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/MaryamNona/ICS2O-Unit2-02-HTML-Basic_Math/actions)
+
+This website can be found at: [https://MaryamNona.github.io/ICS2O-Unit2-02-HTML-Basic_Math](https://MaryamNona.github.io/ICS2O-Unit2-02-HTML-Basic_Math)
---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Maryam Nona
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+function doMathClicked () {
+  // this function does bacic math
+
+  document.getElementById('multiply-math').innerHTML = 'The area is 5 x 3 = ' + (3 * 5)
+  document.getElementById('add-math').innerHTML = 'The perimeter is 2(5 + 3) = ' + (2 (5 + 3))
+}
