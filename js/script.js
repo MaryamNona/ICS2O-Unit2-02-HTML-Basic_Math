033108a61ae00c882a67b258d4012e51a920eede
MaryamNona <>
2021-05-12 03:10:26 +0000

    improved

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@
 // This file contains the JS functions for index.html
 
 function doMathClicked () {
-  // this function does bacic math
+  // this function does basic math
 
-  document.getElementById('multiply-math').innerHTML = 'The area is 5 x 3 = ' + (3 * 5)
-  document.getElementById('add-math').innerHTML = 'The perimeter is 2(5 + 3) = ' + ((5 + 3) * 2)
+  document.getElementById('multiply-math').innerHTML = 'The area is 5 x 3 = ' + (3 * 5) + (' cm2')
+  document.getElementById('add-math').innerHTML = 'The perimeter is 2(5 + 3) = ' + ((5 + 3) * 2) + (' cm')
 }
